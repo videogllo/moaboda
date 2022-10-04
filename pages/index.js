ebf3f72d3a2eb055a6a2e83fe6341b841bcce7df
dongseob <jds3568@naver.com>
2022-10-04 14:14:15 +0900

    deploy test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Videogllo2</a>
+          Welcome to <a href="https://nextjs.org">Videogllo.</a>
         </h1>
 
         <p className={styles.description}>
