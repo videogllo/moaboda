12e64e181f9060c9c59b6fae8c4e2c5e5c7cf574
videogllo.hyejin <hyejin_0410@videogllo.com>
2022-10-21 12:11:50 +0900

    result Page -> Category Event처리 완료 / 상단 광고 Image 완료 / Youtube Result 정렬 중

--- pages/result.js
+++ pages/result.js
@@ -5,6 +5,8 @@ import SearchForm from "./components/searchForm";
 import Keyword from "./components/keyword";
 import Banners from "./components/banners"
 import Category from "./components/category";
+import VideoAd from "./components/videoAd";
+import { LifebuoyIcon } from "@heroicons/react/24/outline";
 
 const Result = (props) => {
     return(
@@ -25,41 +27,34 @@ const Result = (props) => {
                     <Banners></Banners>
                 </div>
                 <div className="relative flex justify-center py-7">
-                    <div className="w-4/5 flex-col justify-center">
-                        <Category></Category>
-                    </div>
+                    <Category></Category>
                 </div>
-                {/* <div className="relative flex justify-center py-7">
-                    <div className="flex">
+                <div className="relative flex justify-center py-7">
+                    <VideoAd></VideoAd>
+                </div>
+                <div className="relative flex justify-center py-7">
+                    <div className="max-w-screen-2xl w-4/5">
                         {props.result[0].youtube.map((i) => (
-                            <div key={i.id} className="flex">
-                                <img src={i.url} width={350}/>
-                                <p>{i.title}</p>
-                                <br />
-                            </div>
-                        ))}
-                    </div>
-                </div> */}
-                {/* <div className="relative flex justify-center py-7">
-                    <div>
-                        <Image src="/image/etc/youtube.svg" width={150} height={40} alt="logo"></Image>
-                        {props.result[0].youtube.map((i) => (
-                            <div key={i.id} className="relative flex items-start py-3">
-                                <img src={i.url} width={350}/>
+                            <div key={i.id} className="relative flex items-start py-3 border-b">
+                                <img src={i.url} width={350} height={200}/>
                                 <div className="relative h-[190px] py-2 pl-1.5">
-                                    <h4 className="text-xl h-1/4 font-extrabold">{i.title}</h4>
-                                    <small className="absolute bottom-0">{i.date}</small>
-                                    <p className="text-base h-2/5">{i.channel}</p>
-                                    <ul>
-                                        {i.tags.map((j) => {
-                                            <li>{j}</li>
-                                        })}
+                                    <h4 className="relative text-xl h-1/4 font-extrabold">{i.title}</h4>
+                                    <p className="relative text-base">{i.channel}</p>
+                                    <ul className="relative flex">
+                                        <li className="mr-2 bg-slate-200">{i.tags[0]}</li>
+                                        <li className="mr-2 bg-slate-200">{i.tags[1]}</li>
+                                        <li className="mr-2 bg-slate-200">{i.tags[2]}</li>
+                                        <li className="mr-2 bg-slate-200">{i.tags[3]}</li>
                                     </ul>
+                                    <small className="relative">{i.date}</small>
+                                    <div className="relative w-[90px] h-[20px]">
+                                        <Image src="/image/etc/youtube.svg" alt="logo" layout="fill" objectFit='contain'></Image>
+                                    </div>
                                 </div>
                             </div>
                         ))}
                     </div>
-                </div> */}
+                </div>
                 <div className="relative flex justify-center py-7">
                     <div>
                         <Image src="/image/etc/twitch.svg" width={150} height={40} alt="logo"></Image>
@@ -77,10 +72,10 @@ const Result = (props) => {
     )
 }
 
-// export const getServerSideProps = async (context) => {
-//     const res = await fetch(process.env.NEXT_PUBLIC_API_URL + `api/result?q=` + context.query.q);
-//     const data = await res.json();
-//     console.log("클라이언트 데이터 확인 : " + data.result[0].youtube);
-//     return {props: data};
-// }
+export const getServerSideProps = async (context) => {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + `api/result?q=` + context.query.q);
+    const data = await res.json();
+    console.log("클라이언트 데이터 확인 : " + data.result[0].youtube);
+    return {props: data};
+}
 export default Result;
