d176150f17195f1382220e8b605b6940b9e24631
videogllo.hyejin <hyejin_0410@videogllo.com>
2022-10-21 16:52:10 +0900

    twitch 검색 결과 데이터 나오도록 설정 및 검색 결과 없을 경우 삼항 연산자 사용하여 아무것도 뜨지 않도록 설정 완료

--- pages/api/result.js
+++ pages/api/result.js
@@ -2,6 +2,8 @@ export default async function handler(req, res) {
     let results = [];
     let videoId = [];
     let youtube = [];
+    let twitch = [];
+    let twitchCateIds = [];
 
     await fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&type=video&key=AIzaSyBLizbrwv_ltQLAD0Y4ovNP9HR1855hj18&q=" + req.query.q)
     .then((response) => response.json())
@@ -18,38 +20,83 @@ export default async function handler(req, res) {
         .then((response) => response.json())
         .then((data) => {
             let items = data.items;
-            console.log(items[0].snippet.tags);
-            youtube.push({id: items[0].id, url: items[0].snippet.thumbnails.medium.url, title: items[0].snippet.title, channel: items[0].snippet.channelTitle, date: items[0].snippet.publishedAt, tags: items[0].snippet.tags});
+            let temp = ['', '', '']
+
+
+            if(items[0].snippet.tags == undefined){
+                youtube.push({id: items[0].id, url: items[0].snippet.thumbnails.medium.url, title: items[0].snippet.title, channel: items[0].snippet.channelTitle, date: items[0].snippet.publishedAt, tags: temp});
+            } else {
+                youtube.push({id: items[0].id, url: items[0].snippet.thumbnails.medium.url, title: items[0].snippet.title, channel: items[0].snippet.channelTitle, date: items[0].snippet.publishedAt, tags: items[0].snippet.tags});
+            }
         });
     }
     results.push({youtube:youtube});
 
 
 
-    await fetch("https://api.twitch.tv/helix/search/channels?first=5&query=" + req.query.q, {
+    await fetch("https://api.twitch.tv/helix/search/categories?query=" + req.query.q, {
         method: 'get',
         headers: {
-            'Authorization':'Bearer oimn0lk86ydwcxvqacucwhzfkq8hjo',
+            'Authorization':'Bearer 09z7q6lphf4nrmu5rjfihquov5orow',
+            'Client-Id':'g901hktaiu6c4v5dt4vkjoptq5vjtk'
         }
     })
     .then((response) => response.json())
     .then((data) => {
         let items = data.data;
-        let twitch = [];
 
-        console.log(data);
-
-        if(data.status = 401){
-            
-        } else {
-            for(let i = 0; i < items.length; i++){
-                if(items[i].title != ''){
-                    twitch.push({id: items[i].id, url: items[i].thumbnail_url.replace("{width}x{height}", "440x240"), title: items[i].title});
-                }
-            }
+        for(let i = 0; i < items.length; i++){
+            twitchCateIds.push({id: items[i].id});
         }
-        results.push({twitch:twitch});
     });
 
+    if(twitchCateIds.length < 1){
+        await fetch("https://api.twitch.tv/helix/search/channel?query=" + req.query.q, {
+        method: 'get',
+        headers: {
+            'Authorization':'Bearer 09z7q6lphf4nrmu5rjfihquov5orow',
+            'Client-Id':'g901hktaiu6c4v5dt4vkjoptq5vjtk'
+        }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            let items = data.data;
+
+            if(data.status != 404){
+                for(let i = 0; i < items.length; i++){
+                    twitchCateIds.push({id: items[i].game_id});
+                }
+            }
+        });
+    }
+    
+    for(let i = 0; i < twitchCateIds.length; i++){
+        await fetch("https://api.twitch.tv/helix/streams?language=ko&first=5&game_id=" + twitchCateIds[i].id, {
+            method: 'get',
+            headers: {
+                'Authorization':'Bearer 09z7q6lphf4nrmu5rjfihquov5orow',
+                'Client-Id':'g901hktaiu6c4v5dt4vkjoptq5vjtk'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            let items = data.data;
+
+            for(let i = 0; i < items.length; i++){
+                twitch.push({id: items[i].id, url: items[i].thumbnail_url.replace('{width}x{height}', '350x200'), title: items[i].title, channel: items[i].user_name, date: items[i].started_at});
+            }
+        });
+    }
+    results.push({twitch:twitch});
+
+    if(youtube.length < 1){
+        youtube.push({id: '', url: '', title: '', channel: '', date: '', tags: ''});
+        results.push({youtube:youtube});
+    }
+
+    if(twitch.length < 1){
+        twitch.push({id: '', url: '', title: '', channel: '', date: ''});
+        results.push({twitch:twitch});
+    }
     res.send({result: results});
 }
--- pages/result.js
+++ pages/result.js
@@ -6,9 +6,11 @@ import Keyword from "./components/keyword";
 import Banners from "./components/banners"
 import Category from "./components/category";
 import VideoAd from "./components/videoAd";
+import Footer from "./components/footer";
 import { LifebuoyIcon } from "@heroicons/react/24/outline";
 
 const Result = (props) => {
+
     return(
         <>
             <div className="bg-slate-100">
@@ -32,41 +34,54 @@ const Result = (props) => {
                 <div className="relative flex justify-center py-7">
                     <VideoAd></VideoAd>
                 </div>
-                <div className="relative flex justify-center py-7">
+                <div className="relative flex justify-center">
                     <div className="max-w-screen-2xl w-4/5">
-                        {props.result[0].youtube.map((i) => (
+                        {props.result[0].youtube[0].id != '' ? props.result[0].youtube.map((i) => (
                             <div key={i.id} className="relative flex items-start py-3 border-b">
                                 <img src={i.url} width={350} height={200}/>
                                 <div className="relative h-[190px] py-2 pl-1.5">
                                     <h4 className="relative text-xl h-1/4 font-extrabold">{i.title}</h4>
                                     <p className="relative text-base">{i.channel}</p>
-                                    <ul className="relative flex">
-                                        <li className="mr-2 bg-slate-200">{i.tags[0]}</li>
-                                        <li className="mr-2 bg-slate-200">{i.tags[1]}</li>
-                                        <li className="mr-2 bg-slate-200">{i.tags[2]}</li>
-                                        <li className="mr-2 bg-slate-200">{i.tags[3]}</li>
+                                    <ul className="relative flex mt-6 mb-5">
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[0]}</li>
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[1]}</li>
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[2]}</li>
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[3]}</li>
                                     </ul>
-                                    <small className="relative">{i.date}</small>
                                     <div className="relative w-[90px] h-[20px]">
                                         <Image src="/image/etc/youtube.svg" alt="logo" layout="fill" objectFit='contain'></Image>
                                     </div>
+                                    <small className="relative">{i.date}</small>
                                 </div>
                             </div>
-                        ))}
+                        )) : ''}
                     </div>
                 </div>
-                <div className="relative flex justify-center py-7">
-                    <div>
-                        <Image src="/image/etc/twitch.svg" width={150} height={40} alt="logo"></Image>
-                        {/* {props.result[1].twitch.map((i) => (
-                            <div key={i.id}>
-                                <img src={i.url} />
-                                <p>{i.title}</p>
-                                <br />
+                <div className="relative flex justify-center">
+                    <div className="max-w-screen-2xl w-4/5">
+                        {props.result[1].twitch[0].id != '' ? props.result[1].twitch.map((i) => (
+                            <div key={i.id} className="relative flex items-start py-3 border-b">
+                                <img src={i.url} width={350} height={200}/>
+                                <div className="relative h-[190px] py-2 pl-1.5">
+                                    <h4 className="relative text-xl h-1/4 font-extrabold">{i.title}</h4>
+                                    <p className="relative text-base">{i.channel}</p>
+                                    <ul className="relative flex mt-6 mb-5">
+                                        {/* <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[0]}</li>
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[1]}</li>
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[2]}</li>
+                                        <li className="mr-2 bg-slate-200 text-blue-800 text-sm rounded-full px-2 pt-0.5">{i.tags[3]}</li> */}
+                                    </ul>
+                                    <div className="relative w-[70px] h-[40px]">
+                                        <Image src="/image/etc/twitch.svg" alt="logo" layout="fill" objectFit='contain'></Image>
+                                    </div>
+                                    <small className="relative">{i.date}</small>
+                                </div>
                             </div>
-                        ))} */}
+                        )) : ''}
                     </div>
                 </div>
+                <div className="h-40"></div>
+                <Footer></Footer>
             </div>
         </>
     )
@@ -75,7 +90,6 @@ const Result = (props) => {
 export const getServerSideProps = async (context) => {
     const res = await fetch(process.env.NEXT_PUBLIC_API_URL + `api/result?q=` + context.query.q);
     const data = await res.json();
-    console.log("클라이언트 데이터 확인 : " + data.result[0].youtube);
     return {props: data};
 }
 export default Result;
