fa72c3860c67ec6ae39c7533220d024e062fdc5c
videogllo <jds3567@videogllo.com>
2022-10-25 10:59:54 +0900

    remove mainListPopular_copy

--- pages/api/main.js
+++ pages/api/main.js
@@ -1,28 +1,29 @@
 export default async function handler(req, res) {
     let results = [];
 
-    await fetch(
-        "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBLizbrwv_ltQLAD0Y4ovNP9HR1855hj18&part=snippet&chart=mostPopular&regionCode=kr&maxResults=10"
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            let items = data.items;
+    // await fetch(
+    //     "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBLizbrwv_ltQLAD0Y4ovNP9HR1855hj18&part=snippet,contentDetails&chart=mostPopular&regionCode=kr&maxResults=10"
+    // )
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         let items = data.items;
 
-            if (items) {
-                for (let i = 0; i < items.length; i++) {
-                    results.push({
-                        id: items[i].id,
-                        title: items[i].snippet.title,
-                        channelName: items[i].snippet.channelTitle,
-                        url: items[i].snippet.thumbnails.standard.url,
-                        uploadDate: items[i].snippet.publishedAt,
-                        thumbnailUrl: items[i].snippet.thumbnails.standard.url,
-                        tag: items[0].snippet.tags,
-                    });
-                    // youtube.push({id: items[i].snippet.channelId, url: items[i].snippet.thumbnails.standard.url, title: items[i].snippet.title});
-                }
-            }
-        });
+    //         if (items) {
+    //             for (let i = 0; i < items.length; i++) {
+    //                 results.push({
+    //                     id: items[i].id,
+    //                     title: items[i].snippet.title,
+    //                     channelName: items[i].snippet.channelTitle,
+    //                     url: items[i].snippet.thumbnails.standard.url,
+    //                     uploadDate: items[i].snippet.publishedAt,
+    //                     thumbnailUrl: items[i].snippet.thumbnails.standard.url,
+    //                     tag: items[0].snippet.tags,
+    //                     playTime: 
+    //                 });
+    //                 // youtube.push({id: items[i].snippet.channelId, url: items[i].snippet.thumbnails.standard.url, title: items[i].snippet.title});
+    //             }
+    //         }
+    //     });
 
     await fetch("https://api.twitch.tv/helix/streams?language=ko&first=9", {
         method: "get",
