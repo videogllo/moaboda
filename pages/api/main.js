4f9927d5d3aa9222bfc5b78f412cd4b9554ca3e9
videogllo <jds3567@videogllo.com>
2022-10-25 10:54:38 +0900

    title명 변경

--- pages/api/main.js
+++ pages/api/main.js
@@ -1,56 +1,65 @@
-export default async function handler(req, res){
+export default async function handler(req, res) {
     let results = [];
 
-    await fetch('https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBLizbrwv_ltQLAD0Y4ovNP9HR1855hj18&part=snippet&chart=mostPopular&regionCode=kr&maxResults=9')
-    .then((response) => response.json())
-    .then((data) => {
-        let items = data.items;
-        let youtube = [];
+    await fetch(
+        "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBLizbrwv_ltQLAD0Y4ovNP9HR1855hj18&part=snippet&chart=mostPopular&regionCode=kr&maxResults=10"
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            let items = data.items;
 
-        /**
-         * dongseob - 빈값 에러 분기 추가
-         * channelId - 채널ID
-         * thumbnails.크기.url - 썸네일의 크기와 url
-         * title - 영상 제목
-         */
-        if(items){
-            for(let i = 0; i < items.length; i++){
-                youtube.push({id: items[i].snippet.channelId, url: items[i].snippet.thumbnails.standard.url, title: items[i].snippet.title});
-            }
-        }
-        // console.log('main으로 보낼 최종 youtube 데이터! : ', youtube);
-        results.push({youtube:youtube});
-    });
-
-    await fetch("https://api.twitch.tv/helix/streams?language=ko&first=9", {
-        method: 'get',
-        headers: {
-            'Authorization':'Bearer oimn0lk86ydwcxvqacucwhzfkq8hjo',
-            'Client-Id':'g901hktaiu6c4v5dt4vkjoptq5vjtk'
-        }
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        let items = data.data;
-        let twitch = [];
-
-        /**
-         * dongseob - 빈값 에러 분기 추가
-         * id - 영상ID
-         * thumbnail_url - 썸네일의 크기와 url ({width}x{height}값을 변경해주어 사용)
-         * title - 영상 제목
-         */
-        if(items){
-            for(let i = 0; i < items.length; i++){
-                if(items[i].title != ''){
-                    twitch.push({id: items[i].id, url: items[i].thumbnail_url.replace("{width}x{height}", "640x480"), title: items[i].title})
+            if (items) {
+                for (let i = 0; i < items.length; i++) {
+                    results.push({
+                        id: items[i].id,
+                        title: items[i].snippet.title,
+                        channelName: items[i].snippet.channelTitle,
+                        url: items[i].snippet.thumbnails.standard.url,
+                        uploadDate: items[i].snippet.publishedAt,
+                        thumbnailUrl: items[i].snippet.thumbnails.standard.url,
+                        tag: items[0].snippet.tags,
+                    });
+                    // youtube.push({id: items[i].snippet.channelId, url: items[i].snippet.thumbnails.standard.url, title: items[i].snippet.title});
                 }
             }
-        }
-        results.push({twitch:twitch});
-    });
+        });
+
+    await fetch("https://api.twitch.tv/helix/streams?language=ko&first=9", {
+        method: "get",
+        headers: {
+            Authorization: "Bearer oimn0lk86ydwcxvqacucwhzfkq8hjo",
+            "Client-Id": "g901hktaiu6c4v5dt4vkjoptq5vjtk",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            let items = data.data;
+            let twitch = [];
+
+            /**
+             * dongseob - 빈값 에러 분기 추가
+             * id - 영상ID
+             * thumbnail_url - 썸네일의 크기와 url ({width}x{height}값을 변경해주어 사용)
+             * title - 영상 제목
+             */
+            if (items) {
+                for (let i = 0; i < items.length; i++) {
+                    if (items[i].title != "") {
+                        twitch.push({
+                            id: items[i].id,
+                            url: items[i].thumbnail_url.replace(
+                                "{width}x{height}",
+                                "640x480"
+                            ),
+                            title: items[i].title,
+                        });
+                    }
+                }
+            }
+            results.push({ twitch: twitch });
+        });
 
     // console.log(results)
 
-    res.send({result: results});
+    res.send({ result: results });
 }
