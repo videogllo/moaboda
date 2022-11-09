75c3a6255f45572e22550de9ed69b3669c269efa
videogllo.hyejin <hyejin_0410@videogllo.com>
2022-11-09 18:42:44 +0900

    TMDB Netflix popular data save  Success

--- pages/api/main.js
+++ pages/api/main.js
@@ -1,4 +1,5 @@
 import * as Common from "../../js/common";
+const axios = require("axios");
 
 export default async function handler(req, res) {
     let results = [];
@@ -64,6 +65,52 @@ export default async function handler(req, res) {
                 }
             }
         });
+        
+        // TMDB
 
+        await axios.all([axios.get("https://api.themoviedb.org/3/movie/popular?api_key=e134931ac620c57b8200b44c971fd541&language=ko-KR&page=1"), axios.get("https://api.themoviedb.org/3/movie/popular?api_key=e134931ac620c57b8200b44c971fd541&language=ko-KR&page=2")])
+        .then(
+            await axios.spread(async(res1, res2) => {
+                for(let i = 0; i < res1.data.results.length; i++){
+                    await axios.get("https://api.themoviedb.org/3/movie/" + res1.data.results[i].id + "?api_key=e134931ac620c57b8200b44c971fd541&language=ko")
+                    .then(function(resp) {
+                        if((resp.data.homepage).includes('netflix')){
+                            console.log(resp.data);
+                            results.push({
+                                id: resp.data.id,                                                                       //영상 아이디
+                                title: resp.data.title,                                                                 //영상 제목
+                                href: resp.data.homepage,                                                               //영상 주소
+                                imgUrl: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/'+resp.data.poster_path,   //썸네일 경로
+                                channel: '/image/ott/neflix.svg',                                                       //채널명
+                                quality: resp.data.popularity,                                                          //총 재생수
+                                date: resp.data.release_date,                                                           //업로드일
+                                tag: resp.data.genres,                                                                  //영상에 등록된 태그
+                                platform: "Netflix",                                                                    //플랫폼 종류
+                            });
+                        }
+                    })
+                }
+                for(let i = 0; i < res2.data.results.length; i++){
+                    await axios.get("https://api.themoviedb.org/3/movie/" + res2.data.results[i].id + "?api_key=e134931ac620c57b8200b44c971fd541&language=ko")
+                    .then(function(resp) {
+                        if((resp.data.homepage).includes('netflix')){
+                            console.log(resp.data);
+                            results.push({
+                                id: resp.data.id,                                                                       //영상 아이디
+                                title: resp.data.title,                                                                 //영상 제목
+                                href: resp.data.homepage,                                                               //영상 주소
+                                imgUrl: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/'+resp.data.poster_path,   //썸네일 경로
+                                channel: '/image/ott/neflix.svg',                                                       //채널명
+                                quality: resp.data.popularity,                                                          //총 재생수
+                                date: resp.data.release_date,                                                           //업로드일
+                                tag: resp.data.genres,                                                                  //영상에 등록된 태그
+                                platform: "Netflix",                                                                    //플랫폼 종류
+                            });
+                        }
+                    })
+                }
+
+            })
+        )
     res.send({ result: results });
 }
